750e187bcc1556df7cd0592eaac7f89effb0c40f
Antonio <rodrigowserra@gmail.com>
2023-09-26 14:37:49 +0100

    Articles layout

--- src/pages/Articles/Articles.jsx
+++ src/pages/Articles/Articles.jsx
@@ -1,50 +1,29 @@
 import React , { useEffect, useState } from 'react';
 import axios from 'axios';
 import CardList from '../../components/CardList/CardList';
-import OtherList from '../../components/OtherList/OtherList';
 import './Articles.css'
 
 function Articles(){
 
     const [articles, setArticles] = useState([]);
-    const [articlesOther, setArticlesOther] = useState([]);
 
     useEffect(() => {
         const apiUrl = 'https://dev.to/api/articles?username=antoniorws';
-
         axios.get(apiUrl)
         .then(response => {
-            const articleData = response.data;
-            const threeArticles = [];
-            const moreArticles = [];
-            let count = 0;
-            for (count; count < articleData.length; count++) {
-                if(count < 6){
-                    threeArticles.push(articleData[count]);
-                }else{
-                    moreArticles.push(articleData[count]);
-                }
-            }
-            setArticles(threeArticles);
-            setArticlesOther(moreArticles);
+            setArticles(response.data);
         })
         .catch(error => {
             console.error('Error to get articles: ', error);
         });
     }, []);
 
-    return <div>
-                <ul class="articles">
-                    {articles.map(article => (
-                        <CardList article = {article}/>
-                    ))}
-                </ul>
-                <ul class="articlesListItem">
-                    {articlesOther.map(article => (
-                    <OtherList article = {article}/>
+    return <ul class="articles">
+                {articles.map(article => (
+                    <CardList article = {article}/>
                 ))}
-                </ul>
-            </div>
+            </ul>
+           
 };
 
 export default Articles;
