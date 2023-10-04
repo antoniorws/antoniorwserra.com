f2dfd58dd6e3a3e1184f1265c66a7bde971423e0
Antonio <rodrigowserra@gmail.com>
2023-10-04 11:11:03 +0100

    update structure folder images

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import './Footer.css';
-import gitHubImage from '../../github.png';
-import linkedInImage from '../../linkedin.png';
-import instagramImage from '../../instagram.png';
+import gitHubImage from '../../assets/images/github.png';
+import linkedInImage from '../../assets/images/linkedin.png';
+import instagramImage from '../../assets/images/instagram.png';
 
 function Footer(){
     return (
         <footer>
             <nav>
                 <ul>
-                    <li><a target='_blank' href="https://github.com/antoniorws"><img src={gitHubImage} /></a></li>
-                    <li><a target='_blank' href="https://www.linkedin.com/in/antonio-rodrigo-wanderley-serra/"><img src={linkedInImage} /></a></li>
-                    <li><a target='_blank' href="https://www.instagram.com/antoniowserra/"><img src={instagramImage} /></a> </li>
+                    <li><a target='_blank' rel='noreferrer' href="https://github.com/antoniorws"><img alt='GitHub' src={gitHubImage} /></a></li>
+                    <li><a target='_blank' rel='noreferrer' href="https://www.linkedin.com/in/antonio-rodrigo-wanderley-serra/"><img alt='LinkedIn' src={linkedInImage} /></a></li>
+                    <li><a target='_blank' rel='noreferrer' href="https://www.instagram.com/antoniowserra/"><img alt='Instagram' src={instagramImage} /></a> </li>
                 </ul>
             </nav>
         </footer>
