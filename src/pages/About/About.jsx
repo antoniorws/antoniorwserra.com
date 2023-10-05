4b6724c3ee9136226ed9cdb6963584784d87249a
Antonio <rodrigowserra@gmail.com>
2023-10-05 23:37:18 +0100

    improve about layout

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import './About.css';
+import aboutImg from '../../assets/images/about.jpg';
 
 function About(){
     return (  
       <main>
         <div class="about">
             <p class="title">About me</p>
-            <div>
-                <p class="one">Very focused professional and very easy to work in a team.</p>
-                <p class="two">I always look for innovation. On my journey I was part of MV Sistemas.</p>
-                <p class="three">Learning the business rules of the Hospital area (mainly the Supplies area).</p>
-                <p class="four">I was also part of the technology area at Accenture focused on financial services, today I am part of China Systems.</p>
+            <div class= "content">
+              <div class="div-image-about">
+                <img src={aboutImg} alt="About Image" />
+              </div>
+              <div>
+                  <p>Very focused professional and very easy to work in a team.</p>
+                  <p>I always look for innovation. On my journey I was part of MV Sistemas.</p>
+                  <p>Learning the business rules of the Hospital area (mainly the Supplies area).</p>
+                  <p>I was also part of the technology area at Accenture focused on financial services, today I am part of China Systems.</p>
+              </div>
             </div>
         </div>
       </main>
