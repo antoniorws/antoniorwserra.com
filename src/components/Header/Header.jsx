7785de16e09babda7d9b4187ae5398f0a1b0e9be
Antonio <rodrigowserra@gmail.com>
2023-10-13 15:57:55 +0100

    refactor - implementing styled components in about

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,33 +1,6 @@
 import React from 'react';
 import styled from "styled-components";
 
-const TabsNav = {
-  position: "relative"
-}
-
-const TabsHighlight = styled.div`
-  background: hsl(0deg 1.89% 14.06% / 87%);
-  position: absolute;
-  top: 9px;
-  left: 0;
-  border-radius: 4px;
-  height: 32px;
-  transition: 0.15s ease;
-  transition-property: width, transform, opacity;
-`;
-
-const Tab = styled.a`
-  padding: 16px 12px;
-  font-size: large;
-  color: hsl(0deg 6.65% 82.62%);
-  display: inline-block;
-  position: relative;
-  cursor: pointer;
-  transition: color 250ms;
-  font-weight: bold;
-  text-decoration: none;
-`;
-
 const tabsData = [
   {
     title: "Home",
@@ -76,15 +49,42 @@ const Header = () => {
   }
 
   return (
-    <header  style={TabsNav} ref={wrapperRef} onMouseLeave={resetHighlight}>
+    <TabsNav ref={wrapperRef} onMouseLeave={resetHighlight}>
       <TabsHighlight ref={highlightRef} style={highlightStyles} />
       {tabsData.map((tab) => (
         <Tab href={tab.href} key={tab.value} onMouseOver={(ev) => repositionHighlight(ev, tab)}>
           {tab.title}
         </Tab>
       ))}
-    </header>
+    </TabsNav>
   );
 };
+
+const TabsNav = styled.header`
+  position: relative;
+`;
+
+const TabsHighlight = styled.div`
+  background: hsl(0deg 1.89% 14.06% / 87%);
+  position: absolute;
+  top: 9px;
+  left: 0;
+  border-radius: 4px;
+  height: 40px;
+  transition: 0.15s ease;
+  transition-property: width, transform, opacity;
+`;
+
+const Tab = styled.a`
+  padding: 16px 12px;
+  font-size: large;
+  color: hsl(0deg 6.65% 82.62%);
+  display: inline-block;
+  position: relative;
+  cursor: pointer;
+  transition: color 250ms;
+  font-weight: bold;
+  text-decoration: none;
+`;
 
 export default Header;
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,27 +1,55 @@
 import React from 'react';
-import './About.css';
+import styled from "styled-components";
 import aboutImg from '../../assets/images/about.jpg';
 import { inject } from '@vercel/analytics';
 
 function About(){
     inject();
     return (  
-      <main>
-        <main class="main-about">
-            <div class= "content">
-              <div class="div-image-about">
-                <img src={aboutImg} alt="About Image" />
-              </div>
+        <MainAbout>
+              <DivImageAbout>
+                <DivImageAboutImg src={aboutImg} alt="About Image" />
+              </DivImageAbout>
               <div>
                   <p>Very focused professional and very easy to work in a team.</p>
                   <p>I always look for innovation. On my journey I was part of MV Sistemas.</p>
                   <p>Learning the business rules of the Hospital area (mainly the Supplies area).</p>
                   <p>I was also part of the technology area at Accenture focused on financial services, today I am part of China Systems.</p>
               </div>
-            </div>
-        </main>
-      </main>
+        </MainAbout>
     )
 }
+
+const MainAbout = styled.main`
+  margin: 0em 0em 5em 0em;
+  font-size: 1em;
+  text-align: justify;
+  background: linear-gradient(135deg, #1d1d1d 0%, #000000 20%);
+  border-radius: 70px;
+  padding: 2em;
+
+  @media screen and (min-width: 700px) {
+    margin: 0em 5em 5em 5em;
+    display: flex;
+    text-align: justify;
+    align-items: center;
+  }
+`;
+
+const DivImageAbout = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
+
+const DivImageAboutImg = styled.img`
+  width: 80%;
+  border-radius: 70px;
+
+  @media screen and (min-width: 700px) {
+    width: 50%;
+    border-radius: 70px;
+  }
+`;
 
 export default About;
